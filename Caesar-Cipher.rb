6c23ca9d8a8f6bcedc65f00a9bbd80a9cbe2da70
Colin Bethea <colinjbethea@gmail.com>
2019-05-29 18:49:52 -0400

    Update Caesar-Cipher.rb

--- Caesar-Cipher.rb
+++ Caesar-Cipher.rb
@@ -1,9 +1,9 @@
 # Defines the caesar_cipher function with parameters user_string (the message) and shift factor (how many letters it will shift to encrypt)
-def caesar_cipher(user_string, shift_factor)
+def caesar_cipher(user_string, shift_amt)
   # If the user_string is lowercase,
   user_string.length.times do |index|
     if /[a-z]/.match(user_string[index])
-      letter = user_string[index].ord + shift_factor
+      letter = user_string[index].ord + shift_amt
       if letter > "z".ord
         user_string[index] = (letter - 26).chr
       else
@@ -11,7 +11,7 @@ def caesar_cipher(user_string, shift_factor)
       end
       # Else if the user_string is uppercase
     elsif /[A-Z]/.match(user_string[index])
-      letter = user_string[index].ord + shift_factor
+      letter = user_string[index].ord + shift_amt
       if letter > "Z".ord
         user_string[index] = (letter - 26).chr
       else
@@ -32,8 +32,8 @@ while true
     break
   else
     puts "What shift factor would you like to use? "
-    shift_factor = gets.to_i # Gathers shift factor in integer form
-    user_string = caesar_cipher(user_string, shift_factor) #Applies function caesar_cipher to user_string with new shift_factor
+    shift_amt = gets.to_i # Gathers shift factor in integer form
+    user_string = caesar_cipher(user_string, shift_amt) #Applies function caesar_cipher to user_string with new shift_factor
     puts user_string # Puts shifted user string in console
   end
 end
